30dccf1f67bb0608bec5ada32ceb3255188bfc5a
Ridwan805 <ridwanurrahman214@gmail.com>
2024-01-01 18:49:48 +0600

    new file

---
+++ use of % , OR & AND operator/4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int x;
+    printf("Enter: ");
+    scanf("%d", &x);
+
+    if (((x % 2) == 0) && ((x % 5) == 0)){
+
+        printf("%d", x);
+
+    }
+    else {
+
+      printf("Not multiple of 2 and 5 both");
+
+    }
+    return 0;
+}
